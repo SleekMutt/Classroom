f28f4963787ffb206b818b15630027a48472ea61
Bohdan <dennger228@gmail.com>
2024-04-16 20:54:19 +0300

    Added exceptions to verify that user is in the organization before creating repository

---
+++ src/main/java/com/example/classroom/exception/UserOrganizationAbsenceException.java
@@ -0,0 +1,8 @@
+package com.example.classroom.exception;
+
+
+public class UserOrganizationAbsenceException extends RuntimeException{
+  public UserOrganizationAbsenceException(String message) {
+    super(message);
+  }
+}
