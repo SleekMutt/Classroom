c94b4ec10a50b6e3f775af9454b45256eb591b80
Bohdan <dennger228@gmail.com>
2024-04-16 20:50:10 +0300

    Updated user controller to new signatures

--- src/main/java/com/example/classroom/controllers/UserController.java
+++ src/main/java/com/example/classroom/controllers/UserController.java
@@ -1,11 +1,12 @@
 package com.example.classroom.controllers;
 
-import com.example.classroom.entities.User;
+import com.example.classroom.dto.user.UserDTO;
 import com.example.classroom.service.user.UserServiceImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -22,24 +23,24 @@ import java.util.List;
 public class UserController {
   @Autowired
   private UserServiceImpl userServiceImpl;
+  @PreAuthorize("hasRole('ADMIN')")
   @GetMapping("/")
-  public ResponseEntity<List<User>> getAllUsers()  {
+  public ResponseEntity<List<UserDTO>> getAllUsers()  {
     return new ResponseEntity<>(userServiceImpl.getAllUsers(), HttpStatus.OK);
   }
-
   @GetMapping("/{id}")
-  public ResponseEntity<User> getUserById(@PathVariable("id") Long id)  {
+  public ResponseEntity<UserDTO> getUserById(@PathVariable("id") Long id)  {
     return new ResponseEntity<>(userServiceImpl.getUserById(id), HttpStatus.OK);
   }
-
+  @PreAuthorize("hasRole('ADMIN')")
   @DeleteMapping("/{id}")
   public ResponseEntity<HttpStatus> deleteUser(@PathVariable("id") Long id)  {
     userServiceImpl.deleteUser(id);
     return new ResponseEntity<>(HttpStatus.NO_CONTENT);
   }
-
+  @PreAuthorize("hasRole('ADMIN')")
   @PutMapping("/")
-  public ResponseEntity<User> updateUser(@RequestBody User user)  {
+  public ResponseEntity<UserDTO> updateUser(@RequestBody UserDTO user)  {
     return new ResponseEntity<>(userServiceImpl.updateUser(user), HttpStatus.OK);
   }
 }
